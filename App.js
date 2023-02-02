769d7ee153ff843d8e6e0fa88c6c539b7686b18d
Tjtechie <tejaswita22mar@gmail.com>
2023-02-03 00:42:43 +0530

    create jsx, configure babel

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
-const heading1 = React.createElement("h1", {id:"title1",  key:"key1"}, "Heading 1");
-const heading2 = React.createElement("h2", {id:"title2", key:"key2"}, "Heading2");
-const containerDiv = React.createElement("div", {id:"container"}, [heading1, heading2]);
+import ReactDOM from "react-dom/client";
+const Title = () => <h1 id="title" key="h2">Title</h1>
+const Header = () => {
+    return (
+        <div>
+            <Title></Title>
+            <h1>Hello functional component</h1>
+            <h2>Heading2</h2>
+        </div>
+    );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(containerDiv);
+root.render(<Header/>);
