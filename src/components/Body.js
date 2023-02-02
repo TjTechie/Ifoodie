70ce8c9eced29edaa3fadcc1873b4ed77f172a71
Tjtechie <tejaswita22mar@gmail.com>
2023-02-03 04:58:19 +0530

    fix the search issues

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,30 +1,49 @@
 import RestaurantCard from "./RestaurantCard";
-import { restaurantList } from "../constants";
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import Shimmer from "./Shimmer";
 
 function filterData(searchText, restaurants ) {
     console.log("hello",searchText,restaurants)
-    const filteredData =  restaurants.filter((restaurant) => restaurant.data.name.includes(searchText));
+    const filteredData =  restaurants.filter((restaurant) => restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase()));
     console.log("filterd",  filteredData)
      return filteredData;
 }
 
 const Body = () => {
-    const [ restaurants, setRestaurants ] = useState(restaurantList);
     const [ searchText, setSearchText ] = useState("KFC");
+    const [allRestaurants, setAllRestaurants] = useState([]);
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+
+    useEffect(()=> {
+        getRestaurants();
+    },[]);
     
-    return (
+    async function getRestaurants() {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        console.log(json);
+        setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+        setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    }
+
+    //Do not render component if no restaurants
+    if(!allRestaurants) return null;
+    if(filteredRestaurants?.length === 0 ) {
+        return <h1>No restaurants found with your search</h1>
+    }
+     
+    return ( filteredRestaurants.length === 0 ) ? <Shimmer></Shimmer> : (
         <>
         <div className="search-container">
             <input type="text" className="search-input" placeholder="search" value={searchText} onChange={(e) => {setSearchText(e.target.value)}}></input>
             <button className="search-btn" onClick={()=> {
-                const data = filterData( searchText, restaurants );
-                setRestaurants(data);
+                const data = filterData( searchText, allRestaurants );
+                setFilteredRestaurants(data);
             }}>Search</button>
         </div>
         <div className="restaurant-list">
             {
-                restaurants.map( (restaurant) => {
+                filteredRestaurants.map( (restaurant) => {
                     return <RestaurantCard {...restaurant.data} key={restaurant.data.id}></RestaurantCard>
                 })
             }
