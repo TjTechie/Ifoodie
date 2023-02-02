2d7af4e0a66507ae52e6a97acfae528be91d86bd
Tjtechie <tejaswita22mar@gmail.com>
2023-02-02 23:46:51 +0530

    add npm. install parcel, react and and react-dom

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 const heading1 = React.createElement("h1", {id:"title1",  key:"key1"}, "Heading 1");
 const heading2 = React.createElement("h2", {id:"title2", key:"key2"}, "Heading2");
 const containerDiv = React.createElement("div", {id:"container"}, [heading1, heading2]);
